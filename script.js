403fc5b9958bbdf3746bcda15c67f29b97314750
Hüseyin Aydın <100160834+huseyinaydinn@users.noreply.github.com>
2023-01-27 17:54:35 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function menuToggle() {
+  var nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+
+  var toggle = document.getElementById("toggle");
+  toggle.classList.toggle("active");
+}
